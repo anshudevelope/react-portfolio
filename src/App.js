2cb5b9a3ca0856e001860926b3ebc821b1c49b22
Anshu <anshu.maurya7300@gmail.com>
2025-03-14 00:13:57 +0530

    navbar start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import react from "react";
 import "./index.css";
-
+import About from "./Routes/About";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Project from "./Routes/Project";
+import {Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import "./Navbarstyle.css"
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2>I am Navbar</h2>
+    </div>
+  )
+}
+
+export default Navbar
